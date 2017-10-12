4cf243f29ba3e7dc45a81ad64ef0a92ce10fea84
Lawrence Tan <lawreyios@gmail.com>
2017-10-12 12:39:04 +0800

    Reset

---
+++ Workspace/Appcoda/MusicRepeater/MusicRepeater/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MusicRepeater
+//
+//  Created by Lawrence Tan on 12/10/17.
+//  Copyright © 2017 AppCoda. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
